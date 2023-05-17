d1f0a5bf41c026acf30ff098e1b200759af7cfeb
Arran <plazmapie335@gmail.com>
2023-05-17 17:55:37 +1200

    Github API requester

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
+<!--This project is using Axios to make HTTP requests-->
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <GitubReq/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GitubReq from './components/githubAPIComp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GitubReq
   }
 }
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+</script>
---
+++ src/components/githubAPIComp.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <h1>Github API requester</h1>
+        <ul>
+            <!-- Loop through the api request -->
+            <li v-for="project in projects" :key="project.id">
+                <p>{{ project.name }}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            projects: [],
+        };
+    },
+    mounted() {
+        this.fetchProjects();
+    },
+    methods: {
+        fetchProjects() {
+            const userName = 'Arran-Logan-Riley';
+
+            axios
+                .get(`https://api.github.com/users/${userName}/repos`)
+                .then(response => {
+                    this.projects = response.data;
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        },
+    },
+};
+</script>
