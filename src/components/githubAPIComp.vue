4d93d086b2bb176706de944693b4188eae5411aa
Arran <plazmapie335@gmail.com>
2023-05-18 16:29:56 +1200

    Added sort via time and other features

--- src/components/githubAPIComp.vue
+++ src/components/githubAPIComp.vue
@@ -8,10 +8,15 @@
             <input v-model="newUser" type="text" placeholder="Type in user name">
             <button @click="fetchProjects">Submit</button>
         </div>
+        <div><p>Total number of repositories : {{ totNumRepo }}</p></div>
+        <div><p>Link to user: <a v-bind:href="userURL" target="_blank">{{ userURL }}</a></p></div>
         <ul>
             <!-- Loop through the api request -->
             <li v-for="project in projects" :key="project.id">
                 <p>{{ project.name }}</p>
+                <p>{{ project.description }}</p>
+                <p>Created on: {{ project.created_at }}</p>
+                <a v-bind:href="project.clone_url" target="_blank">Link: {{ project.clone_url }}</a>
             </li>
         </ul>
     </div>
@@ -24,7 +29,9 @@ export default {
     data() {
         return {
             projects: [],
-            newUser: ''
+            newUser: '',
+            totNumRepo: '_',
+            userURL: ''
         };
     },
     mounted() {
@@ -36,7 +43,28 @@ export default {
             axios
                 .get(`https://api.github.com/users/${userName}/repos`)
                 .then(response => {
-                    this.projects = response.data;
+                    //Use sorting method to sort data cronologically
+                    const cronoUserSort = response.data.sort((a,b) => {
+                        //create dates a and b objects to compare against eachother
+                        const dateA = new Date(a.created_at);
+                        const dateB = new Date(b.created_at);
+                        //We subtract A from B to determine a numerical number
+                        //If the returned value is negative, a will be placed before b.
+                        //If the returned value is positive, a will be placed after b.
+                        return dateB - dateA;
+                    })
+                    
+                    this.projects = cronoUserSort
+                    this.totNumRepo = cronoUserSort.length
+                    this.userURL = cronoUserSort['0'].owner.html_url
+                    let printData = false
+                    if(printData == true){
+                        for(let i = 0; i < cronoUserSort.length; i++){
+                        console.log(cronoUserSort[i].created_at);
+                    }
+                    
+                    }
+                    
                 })
                 .catch(error => {
                     console.log(error);
