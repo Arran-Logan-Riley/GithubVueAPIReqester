7efaa4ba23d2a465f3ce659083d6277d8d081fcb
Arran <plazmapie335@gmail.com>
2023-05-18 17:15:40 +1200

    Added Css styling and logic fixes

--- src/components/githubAPIComp.vue
+++ src/components/githubAPIComp.vue
@@ -1,22 +1,23 @@
 <template>
     <div>
-        <h1>Github API requester</h1>
+        <h1>Github API Requester</h1>
         <div>
             <label for='name'>Type in a user name</label>
         </div>
-        <div>
-            <input v-model="newUser" type="text" placeholder="Type in user name">
-            <button @click="fetchProjects">Submit</button>
+        <div class="">
+            <input class="input box" v-model="newUser" type="text" placeholder="Type in user name">
+            <button class="button" @click="fetchProjects">Submit</button>
         </div>
         <div><p>Total number of repositories : {{ totNumRepo }}</p></div>
         <div><p>Link to user: <a v-bind:href="userURL" target="_blank">{{ userURL }}</a></p></div>
         <ul>
             <!-- Loop through the api request -->
             <li v-for="project in projects" :key="project.id">
-                <p>{{ project.name }}</p>
+                <h3>{{ project.name }}</h3>
                 <p>{{ project.description }}</p>
                 <p>Created on: {{ project.created_at }}</p>
                 <a v-bind:href="project.clone_url" target="_blank">Link: {{ project.clone_url }}</a>
+                <hr>
             </li>
         </ul>
     </div>
@@ -55,8 +56,16 @@ export default {
                     })
                     
                     this.projects = cronoUserSort
-                    this.totNumRepo = cronoUserSort.length
+                    console.log(cronoUserSort)
+                    //If there is more than 30 repos, change it to 30+. Cannot request more than 30 repos.
+                    if(cronoUserSort.length == 30){
+                        this.totNumRepo = "30+"
+                    }else{
+                        this.totNumRepo = cronoUserSort.length
+                    }
+                    
                     this.userURL = cronoUserSort['0'].owner.html_url
+                    
                     let printData = false
                     if(printData == true){
                         for(let i = 0; i < cronoUserSort.length; i++){
@@ -67,9 +76,73 @@ export default {
                     
                 })
                 .catch(error => {
+                    this.userURL = 'No user found';
+                    this.totNumRepo = 0;
                     console.log(error);
                 });
         },
     },
 };
 </script>
+
+<style>
+body{
+    margin: 0; 
+    padding: 0;
+    background: #19161c;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    flex-direction: column;
+    align-content: center;
+    color: white;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+}
+.box{
+    position: relative;
+}
+.input {
+    padding: 10px;
+    width: 300;
+    height: 40px;
+    background: none;
+    border: 2px solid #ffd52d;
+    border-radius: 50px;
+    box-sizing: border-box;
+    font-size: 26px;
+    color: #ffd52d;
+    outline: none;
+    transition: .5s;
+}
+
+.button{
+    background: none;
+    margin: 10px;
+    height: 40px;
+    border: 2px solid #ffd52d;
+    border-radius: 50px;
+    box-sizing: border-box;
+    font-size: 26px;
+    color: #ffd52d;
+    outline: none;
+    transition: .5s;
+}
+
+a{
+    color: #ffeaa7
+}
+
+.button:hover{
+    background: #3b3640;
+    border-radius: 10px;
+}
+
+.input:hover{
+    background: #3b3640;
+    border-radius: 10px;
+}
+
+#app{
+width: 90%;
+}
+</style>
